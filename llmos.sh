3c4d65dbb5ad0ef2fd4fa9b83ab120cca2bdc90e
m-c-frank <martin7.frank7@gmail.com>
2023-12-05 06:16:16 +0100

    adding git url argument

--- llmos.sh
+++ llmos.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <git-repo-url>"
+    exit 1
+fi
+
+REPO_URL=$1
+
 IMAGE_NAME="llmos"
 PORT_EXTERN=4000
 PORT_INTERN=4000
 
-docker build -t $IMAGE_NAME .
+docker build --build-arg REPO_URL=$REPO_URL -t $IMAGE_NAME .
 
 CONTAINER_USING_PORT=$(docker ps --filter "publish=$PORT_EXTERN" -q)
 if [ ! -z "$CONTAINER_USING_PORT" ]; then
-    echo "Port $PORT_EXTERN is in use by container $CONTAINER_USING_PORT. Stopping and removing it..."
     docker stop $CONTAINER_USING_PORT
     docker rm $CONTAINER_USING_PORT
 fi
