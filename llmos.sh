44436e9541e530a2a021bff8709fa29f5db84def
m-c-frank <martin7.frank7@gmail.com>
2023-12-05 06:00:51 +0100

    stopping old container

--- llmos.sh
+++ llmos.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 IMAGE_NAME="llmos"
-HOST_PORT=4000
-CONTAINER_PORT=4000
+PORT_EXTERN=4000
+PORT_INTERN=4000
 
 docker build -t $IMAGE_NAME .
-CONTAINER_ID=$(docker run -d -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME)
+
+CONTAINER_USING_PORT=$(docker ps --filter "publish=$PORT_EXTERN" -q)
+if [ ! -z "$CONTAINER_USING_PORT" ]; then
+    echo "Port $PORT_EXTERN is in use by container $CONTAINER_USING_PORT. Stopping and removing it..."
+    docker stop $CONTAINER_USING_PORT
+    docker rm $CONTAINER_USING_PORT
+fi
+
+CONTAINER_ID=$(docker run -d -p $PORT_EXTERN:$PORT_INTERN $IMAGE_NAME)
 
 while [ "$(docker inspect --format='{{.State.Running}}' $CONTAINER_ID)" != "true" ]; do
     sleep 1
