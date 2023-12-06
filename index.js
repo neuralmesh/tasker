d6d74e9a56d6f9c0988d3623850ed2e0f6d308b8
m-c-frank <martin7.frank7@gmail.com>
2023-12-06 07:06:24 +0100

    modifying something

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+
+const githubToken = process.env.GH_PAT;
+if (githubToken) {
+    execSync(`echo ${githubToken} | gh auth login --with-token`);
+}
 
 const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Mutation {
+        createIssue(title: String!, body: String!): String
+    }
 `);
 
 const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+    createIssue: ({ title, body }) => {
+        return new Promise((resolve, reject) => {
+            const command = `gh issue create -R [Your-Repo-Name] -t "${title}" -b "${body}"`;
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    reject(`exec error: ${error}`);
+                } else {
+                    resolve(stdout);
+                }
+            });
+        });
+    }
 };
 
 const app = express();
+
 app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
 }));
 
-const port = process.env.CONTAINER_PORT || 4000;
-app.listen(port);
-console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+const port = process.env.CONTAINER_PORT;
+app.listen(port, () => {
+    console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
+});
 
